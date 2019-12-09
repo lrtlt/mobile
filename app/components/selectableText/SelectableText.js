0370724c706710a675d4090098cd49c29f71c714
Kestas Venslauskas <kestas.venslauskas@lamoda.ru>
2019-12-09 13:02:46 +0200

    - Reverted selectable text workaround for iOS because of multiple new bugs introduced.

--- app/components/selectableText/SelectableText.js
+++ app/components/selectableText/SelectableText.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import { Text, TextInput, Platform } from 'react-native';
+import { Text } from 'react-native';
 
 /**
- * Selectable text implementation based on OS.
+ * TODO: find working solution
  * iOS does not support selectable text out of the box.
- * Workaround for now:
- * https://github.com/facebook/react-native/issues/13938#issuecomment-520590673
  */
+
 const SelectableText = props => {
-  if (Platform.OS === 'ios') {
-    return (
-      <TextInput {...props} editable={false} scrollEnabled={false} multiline={true}>
-        {props.children}
-      </TextInput>
-    );
-  } else {
-    return (
-      <Text {...props} selectable={true} textBreakStrategy="simple">
-        {props.children}
-      </Text>
-    );
-  }
+  return (
+    <Text {...props} selectable={true} textBreakStrategy="simple">
+      {props.children}
+    </Text>
+  );
 };
 
 export default SelectableText;
