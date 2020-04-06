1f07b10225468d23c1a18665460e3afe5b8dcbb3
Kestas Venslauskas <kvenslauskas@shift4.com>
2020-04-06 15:28:53 +0300

    RN Firebase migrated to v6

--- android/app/src/main/java/lt/mediapark/lrt/MainApplication.java
+++ android/app/src/main/java/lt/mediapark/lrt/MainApplication.java
@@ -6,7 +6,6 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
-import io.invertase.firebase.fabric.crashlytics.RNFirebaseCrashlyticsPackage;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -21,7 +20,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       @SuppressWarnings("UnnecessaryLocalVariable")
       List<ReactPackage> packages = new PackageList(this).getPackages();
-      packages.add(new RNFirebaseCrashlyticsPackage());
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
       return packages;
