e290ef6cb258153e88226f48163bb4cf1690ce6a
KestasVenslauskas <>
2020-07-31 13:17:44 +0300

    Updated missing vector icons for iOS

--- app/components/svg/ic_comments.ios.js
+++ app/components/svg/ic_comments.ios.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Icon from 'react-native-vector-icons/Ionicons';
 
-const SvgComponent = props => <Icon name="md-chatboxes" size={props.size} color={props.color} />;
+const SvgComponent = props => <Icon name="md-chatbox-outline" size={props.size - 2} color={props.color} />;
 
 export default SvgComponent;
