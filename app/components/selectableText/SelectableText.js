ab28a5ae47a0d7d2f2aae2a2c1fe3a652c8068a3
DESKTOP-85L53SV\Kestas <kestas.venslauskas@gmail.com>
2020-08-26 12:27:50 +0300

    Revert: SelectableText actions feature request. Because of a bug

--- app/components/selectableText/SelectableText.js
+++ app/components/selectableText/SelectableText.js
@@ -1,65 +1,17 @@
 import React from 'react';
-import { Linking } from 'react-native';
-import Clipboard from '@react-native-community/clipboard';
-import qs from 'qs';
-import { SelectableText } from '@astrocoders/react-native-selectable-text';
+import { Text } from 'react-native';
 
 /**
  * TODO: find working solution
  * iOS does not support selectable text out of the box.
  */
 
-const EVENT_COPY = 'Kopijuoti';
-const EVENT_REPORT = 'Pranešti apie klaidą';
-
-const REPORT_EMAIL = 'portalas@lrt.lt';
-const REPORT_EMAIL_SUBJECT = 'Klaida tekste';
-
-const component = props => {
+const SelectableText = props => {
   return (
-    <SelectableText
-      menuItems={[EVENT_COPY, EVENT_REPORT]}
-      textComponentProps={{ ...props, textBreakStrategy: 'simple' }}
-      onSelection={({ eventType, content, selectionStart, selectionEnd }) => {
-        switch (eventType) {
-          case EVENT_COPY: {
-            Clipboard.setString(content);
-            break;
-          }
-          case EVENT_REPORT: {
-            sendEmail(REPORT_EMAIL, REPORT_EMAIL_SUBJECT, content);
-            break;
-          }
-        }
-      }}
-      value={props.children}
-    />
+    <Text {...props} selectable={true} textBreakStrategy="simple">
+      {props.children}
+    </Text>
   );
 };
 
-export async function sendEmail(to, subject, body, options = {}) {
-  const { cc, bcc } = options;
-
-  let url = `mailto:${to}`;
-
-  const query = qs.stringify({
-    subject: subject,
-    body: body,
-    cc: cc,
-    bcc: bcc,
-  });
-
-  if (query.length) {
-    url += `?${query}`;
-  }
-
-  const canOpen = await Linking.canOpenURL(url);
-
-  if (!canOpen) {
-    throw new Error('Provided URL can not be handled');
-  }
-
-  return Linking.openURL(url);
-}
-
-export default component;
+export default SelectableText;
