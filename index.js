eba002423e96ca8643f70ab45262e7f8ec5de88d
Kestas Venslauskas <kestas.venslauskas@gmail.com>
2020-11-23 14:26:19 +0200

    Removed deprecated YellowBox

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 import 'react-native-gesture-handler';
 
 import React from 'react';
-import {AppRegistry, YellowBox} from 'react-native';
+import {AppRegistry} from 'react-native';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import App from './App';
 import {store, persistor} from './app/redux/store';
 import {name as appName} from './app.json';
 import {SafeAreaProvider, initialWindowMetrics} from 'react-native-safe-area-context';
-
-YellowBox.ignoreWarnings(['componentWillMount is deprecated', 'componentWillReceiveProps']);
 
 const reduxProvider = () => (
   <Provider store={store}>
