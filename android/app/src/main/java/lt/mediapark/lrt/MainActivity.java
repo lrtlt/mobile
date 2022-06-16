90408c3766f3f4b9805d9a8bbfc5722965e87eef
Kestas Venslauskas <kestas.venslauskas@gmail.com>
2022-06-16 14:01:51 +0300

    react-native upgraded to 0.68.2

--- android/app/src/main/java/lt/mediapark/lrt/MainActivity.java
+++ android/app/src/main/java/lt/mediapark/lrt/MainActivity.java
@@ -11,26 +11,49 @@ import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
-  @Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(null);
     }
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  @Override
-  protected String getMainComponentName() {
-    return "lrtApp";
-  }
+    /**
+     * Returns the name of the main component registered from JavaScript. This is used to schedule
+     * rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "lrtApp";
+    }
 
-  // Workaround appcompat-1.1.0 bug https://issuetracker.google.com/issues/141132133
-  @Override
-  public void applyOverrideConfiguration(Configuration overrideConfiguration) {
-      if (Build.VERSION.SDK_INT >= 21 && Build.VERSION.SDK_INT <= 25) {
-          return;
-      }
-      super.applyOverrideConfiguration(overrideConfiguration);
-  }
+    /**
+     * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
+     * you can specify the rendered you wish to use (Fabric or the older renderer).
+     */
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new MainActivityDelegate(this, getMainComponentName());
+    }
+
+    public static class MainActivityDelegate extends ReactActivityDelegate {
+        public MainActivityDelegate(ReactActivity activity, String mainComponentName) {
+            super(activity, mainComponentName);
+        }
+
+        @Override
+        protected ReactRootView createRootView() {
+            ReactRootView reactRootView = new ReactRootView(getContext());
+            // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+            reactRootView.setIsFabric(BuildConfig.IS_NEW_ARCHITECTURE_ENABLED);
+            return reactRootView;
+        }
+    }
+
+    // Workaround appcompat-1.1.0 bug https://issuetracker.google.com/issues/141132133
+    @Override
+    public void applyOverrideConfiguration(Configuration overrideConfiguration) {
+        if (Build.VERSION.SDK_INT >= 21 && Build.VERSION.SDK_INT <= 25) {
+            return;
+        }
+        super.applyOverrideConfiguration(overrideConfiguration);
+    }
 }
