bfac893dd8da520a72bdcce6dc2a739cd3fba996
Kestas Venslauskas <kestas.venslauskas@gmail.com>
2024-03-14 17:19:01 +0200

    chore: disable android auto module

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ import 'react-native-gesture-handler';
 import App from './App';
 import {AppRegistry, Platform} from 'react-native';
 import {name as appName} from './app.json';
-import {AndroidAutoModule} from './AndroidAuto';
+// import {AndroidAutoModule} from './AndroidAuto';
 
 if (Platform.OS === 'android') {
-  AppRegistry.registerRunnable('AndroidAuto', AndroidAutoModule);
+  // AppRegistry.registerRunnable('AndroidAuto', AndroidAutoModule);
   AppRegistry.registerComponent(appName, () => App);
 } else {
   AppRegistry.registerComponent(appName, () => App);
