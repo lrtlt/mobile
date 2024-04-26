43a5d94239c0bab58c831534dc753e455140f158
Kestas Venslauskas <kestas.venslauskas@gmail.com>
2024-04-26 14:10:30 +0300

    chore: rn upgrade to 73.7

---
+++ android/app/src/main/java/lt/mediapark/lrt/MainActivity.kt
@@ -0,0 +1,66 @@
+package lt.mediapark.lrt;
+
+import android.content.Intent
+import android.content.res.Configuration
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.google.android.gms.cast.framework.CastContext
+
+class MainActivity : ReactActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(null)
+
+        try {
+            // lazy load Google Cast context
+            CastContext.getSharedInstance(this)
+        } catch (e: Exception) {
+            Log.e("MainActivity", "onCreate: ", e)
+            // cast framework not supported
+        }
+    }
+
+    /**
+     * Returns the name of the main component registered from JavaScript. This is used to schedule
+     * rendering of the component.
+     */
+    override fun getMainComponentName(): String = "lrtApp"
+
+    /**
+     * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+     * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+     */
+    override fun createReactActivityDelegate(): ReactActivityDelegate =
+        DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+    // Workaround appcompat-1.1.0 bug https://issuetracker.google.com/issues/141132133
+    override fun applyOverrideConfiguration(overrideConfiguration: Configuration?) {
+        if (Build.VERSION.SDK_INT <= 25) {
+            return
+        }
+        super.applyOverrideConfiguration(overrideConfiguration)
+    }
+
+    //Theo-player PIP setup
+    override fun onUserLeaveHint() {
+        this.sendBroadcast(Intent("onUserLeaveHint"))
+        super.onUserLeaveHint()
+    }
+
+    override fun onPictureInPictureModeChanged(
+        isInPictureInPictureMode: Boolean,
+        newConfig: Configuration
+    ) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            super.onPictureInPictureModeChanged(isInPictureInPictureMode, newConfig)
+            val intent = Intent("onPictureInPictureModeChanged")
+            intent.putExtra("isInPictureInPictureMode", isInPictureInPictureMode)
+            this.sendBroadcast(intent)
+        }
+    }
+}
--- ios/lrtApp/AppDelegate.swift
+++ ios/lrtApp/AppDelegate.swift
@@ -1,108 +1,117 @@
-import UIKit
 import CarPlay
-import React
-import React_RCTAppDelegate
 import Firebase
 import GoogleCast
+import React
+import React_RCTAppDelegate
+import UIKit
 
 #if DEBUG
-#if FB_SONARKIT_ENABLED
-import FlipperKit
-#endif
+  #if FB_SONARKIT_ENABLED
+    import FlipperKit
+  #endif
 #endif
 
 @main
 class AppDelegate: RCTAppDelegate, GCKLoggerDelegate {
 
-  
-  var rootView: UIView?;
-  var concurrentRootEnabled = true;
+  var rootView: UIView?
+  var concurrentRootEnabled = true
 
   static var shared: AppDelegate { return UIApplication.shared.delegate as! AppDelegate }
 
-  
-  override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]?) -> Bool {
-    FirebaseApp.configure();
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    FirebaseApp.configure()
     let kReceiverAppID = kGCKDefaultMediaReceiverApplicationID
     let criteria = GCKDiscoveryCriteria(applicationID: kReceiverAppID)
     let options = GCKCastOptions(discoveryCriteria: criteria)
     GCKCastContext.setSharedInstanceWith(options)
-  
+
     moduleName = "lrtApp"
-    return true;
-    
-// Moved initialization to scenes
-    
-//    let app = super.application(application, didFinishLaunchingWithOptions: launchOptions);
-//    
-//    
-//    self.rootView = self.createRootView(
-//      with: self.bridge,
-//      moduleName: self.moduleName,
-//      initProps: self.prepareInitialProps()
-//    );
-//    return app;
+    return true
+
+    // Moved initialization to scenes
+
+    //    let app = super.application(application, didFinishLaunchingWithOptions: launchOptions);
+    //
+    //
+    //    self.rootView = self.createRootView(
+    //      with: self.bridge,
+    //      moduleName: self.moduleName,
+    //      initProps: self.prepareInitialProps()
+    //    );
+    //    return app;
   }
-  
+
   func initAppFromScene(connectionOptions: UIScene.ConnectionOptions?) {
-     // If bridge has already been initiated by another scene, there's nothing to do here
-     if (self.bridge != nil) {
-       return;
-     }
+    // If bridge has already been initiated by another scene, there's nothing to do here
+    if self.bridge != nil {
+      return
+    }
 
-     let enableTM = false;
- #if RCT_NEW_ARCH_ENABLED
-     enableTM = self.turboModuleEnabled;
- #endif
+    let enableTM = false
+    #if RCT_NEW_ARCH_ENABLED
+      enableTM = self.turboModuleEnabled
+    #endif
 
-     let application = UIApplication.shared;
-     RCTAppSetupPrepareApp(application, enableTM);
+    let application = UIApplication.shared
+    RCTAppSetupPrepareApp(application, enableTM)
 
-     if (self.bridge == nil) {
-       self.bridge = super.createBridge(
-         with: self,
-         launchOptions: [:]
-       )
-     }
+    if self.bridge == nil {
+      self.bridge = super.createBridge(
+        with: self,
+        launchOptions: [:]
+      )
+    }
 
- #if RCT_NEW_ARCH_ENABLED
-     _contextContainer = UnsafeMutablePointer<ContextContainer>.allocate(capacity: 1)
-     _contextContainer?.initialize(to: ContextContainer())
-     _reactNativeConfig = UnsafeMutablePointer<EmptyReactNativeConfig>.allocate(capacity: 1)
-     _reactNativeConfig?.initialize(to: EmptyReactNativeConfig())
-     _contextContainer?.pointee.insert("ReactNativeConfig", _reactNativeConfig)
-     self.bridgeAdapter = RCTSurfacePresenterBridgeAdapter(bridge: self.bridge, contextContainer: _contextContainer)
-     self.bridge?.surfacePresenter = self.bridgeAdapter?.surfacePresenter
- #endif
+    #if RCT_NEW_ARCH_ENABLED
+      _contextContainer = UnsafeMutablePointer<ContextContainer>.allocate(capacity: 1)
+      _contextContainer?.initialize(to: ContextContainer())
+      _reactNativeConfig = UnsafeMutablePointer<EmptyReactNativeConfig>.allocate(capacity: 1)
+      _reactNativeConfig?.initialize(to: EmptyReactNativeConfig())
+      _contextContainer?.pointee.insert("ReactNativeConfig", _reactNativeConfig)
+      self.bridgeAdapter = RCTSurfacePresenterBridgeAdapter(
+        bridge: self.bridge, contextContainer: _contextContainer)
+      self.bridge?.surfacePresenter = self.bridgeAdapter?.surfacePresenter
+    #endif
 
-     let initProps = self.prepareInitialProps();
-     self.rootView = self.createRootView(with: self.bridge, moduleName: self.moduleName, initProps: initProps)
+    let initProps = self.prepareInitialProps()
+    self.rootView = self.createRootView(
+      with: self.bridge, moduleName: self.moduleName, initProps: initProps)
 
-     if #available(iOS 13.0, *) {
-       self.rootView!.backgroundColor = UIColor.systemBackground
-     } else {
-       self.rootView!.backgroundColor = UIColor.white
-     }
-   }
+    if #available(iOS 13.0, *) {
+      self.rootView!.backgroundColor = UIColor.systemBackground
+    } else {
+      self.rootView!.backgroundColor = UIColor.white
+    }
+  }
 
-  override func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-    if (connectingSceneSession.role == UISceneSession.Role.carTemplateApplication) {
-      let scene =  UISceneConfiguration(name: "CarPlay", sessionRole: connectingSceneSession.role)
+  override func application(
+    _ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession,
+    options: UIScene.ConnectionOptions
+  ) -> UISceneConfiguration {
+    if connectingSceneSession.role == UISceneSession.Role.carTemplateApplication {
+      let scene = UISceneConfiguration(name: "CarPlay", sessionRole: connectingSceneSession.role)
       scene.delegateClass = CarSceneDelegate.self
       return scene
     } else {
-      let scene =  UISceneConfiguration(name: "Phone", sessionRole: connectingSceneSession.role)
+      let scene = UISceneConfiguration(name: "Phone", sessionRole: connectingSceneSession.role)
       scene.delegateClass = PhoneSceneDelegate.self
       return scene
     }
   }
-  
 
-  override func sourceURL(for bridge: RCTBridge!) -> URL! {
+  override func sourceURL(for bridge: RCTBridge!) -> URL {
+    return getBundleURL()
+  }
+
+  func getBundleURL() -> URL {
     #if DEBUG
-      return RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index");
+      return RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
     #else
-      return Bundle.main.url(forResource:"main", withExtension:"jsbundle")
+      return Bundle.main.url(forResource: "main", withExtension: "jsbundle")
     #endif
   }
 
@@ -114,7 +123,7 @@ class AppDelegate: RCTAppDelegate, GCKLoggerDelegate {
     #endif
     return initProps
   }
-  
+
   // Not working any more because it's setup is moved to PhoneScene
   //  override func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
   //      return RCTLinkingManager.application(application, open: url, options: options)
