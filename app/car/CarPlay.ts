8b510f5ea3a004a83ac031e46a780b72b0cfb5d2
Kestas Venslauskas <kestas.venslauskas@gmail.com>
2024-07-23 22:41:59 +0300

    feat: continue CarPlay player on after reconnect

--- app/car/CarPlay.ts
+++ app/car/CarPlay.ts
@@ -1,35 +1,70 @@
 import {AlertTemplate, CarPlay} from 'react-native-carplay';
 import Gemius from 'react-native-gemius-plugin';
 import analytics from '@react-native-firebase/analytics';
-import TrackPlayer from 'react-native-track-player';
+import TrackPlayer, {State} from 'react-native-track-player';
 import {setupTrackPlayer} from './useTrackPlayerSetup';
 
 import RootTemplate from './root/RootTemplate';
+import {carPlayNowPlayingTemplate} from './nowPlaying/createNowPlayingTemplate';
+import {debounce} from 'lodash';
 
-CarPlay.emitter.addListener('didConnect', async () => {
-  console.log('### CarPlay connected');
-  Gemius.sendPageViewedEvent('carplay_connected');
-  analytics().logEvent('carplay_connected');
-  setupTrackPlayer();
+let isPlayerWaitingToResume = false;
 
-  CarPlay.presentTemplate(
-    new AlertTemplate({
-      id: 'loading-alert',
-      titleVariants: ['Prašome palaukti...'],
-    }),
-  );
-  const rootTemplate = await RootTemplate.build();
-  CarPlay.dismissTemplate();
-  CarPlay.setRootTemplate(rootTemplate);
-  CarPlay.enableNowPlaying(true);
-});
+const DEBOUNCE_TIME = 1000;
 
-CarPlay.emitter.addListener('didDisconnect', () => {
-  console.log('### CarPlay disconnected');
-  Gemius.sendPageViewedEvent('carplay_disconnected');
-  analytics().logEvent('carplay_disconnected');
-  TrackPlayer.reset();
-});
+CarPlay.emitter.addListener(
+  'didConnect',
+  debounce(async () => {
+    console.log('### CarPlay connected');
+
+    Gemius.sendPageViewedEvent('carplay_connected');
+    analytics().logEvent('carplay_connected');
+    await setupTrackPlayer();
+
+    CarPlay.presentTemplate(
+      new AlertTemplate({
+        id: 'loading-alert',
+        titleVariants: ['Prašome palaukti...'],
+      }),
+    );
+    const rootTemplate = await RootTemplate.build();
+    CarPlay.dismissTemplate();
+    CarPlay.setRootTemplate(rootTemplate);
+    CarPlay.enableNowPlaying(true);
+
+    if (isPlayerWaitingToResume) {
+      isPlayerWaitingToResume = false;
+      TrackPlayer.getQueue().then((queue) => {
+        console.log('queue', queue);
+        if (queue.length > 0) {
+          CarPlay.pushTemplate(carPlayNowPlayingTemplate, true);
+          TrackPlayer.play();
+        }
+      });
+    }
+  }, DEBOUNCE_TIME),
+);
+
+CarPlay.emitter.addListener(
+  'didDisconnect',
+  debounce(() => {
+    console.log('### CarPlay disconnected');
+
+    Gemius.sendPageViewedEvent('carplay_disconnected');
+    analytics().logEvent('carplay_disconnected');
+
+    TrackPlayer.getPlaybackState()
+      .then((state) => {
+        if (state.state === State.Playing) {
+          isPlayerWaitingToResume = true;
+          TrackPlayer.pause();
+        }
+      })
+      .catch((e) => {
+        //ignore
+      });
+  }, DEBOUNCE_TIME),
+);
 
 CarPlay.emitter.addListener('backButtonPressed', () => {
   console.log('### Back button pressed');
