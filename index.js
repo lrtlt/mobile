2559d7a1d2468aec051d241ce39403dd5d5695c0
Kestas Venslauskas <kestas.venslauskas@gmail.com>
2025-01-09 16:49:56 +0200

    chore: delete old carplay implementation

--- index.js
+++ index.js
@@ -6,24 +6,8 @@ import 'react-native-unistyles';
 import {setupGemius} from './app/util/useGemiusSetup';
 setupGemius();
 
-import {setupTrackPlayer} from './app/car/useTrackPlayerSetup';
-
-// Setup CarPlay
-if (Platform.OS === 'ios') {
-  require('./app/car/CarPlay');
-}
-
 import App from './App';
 import {AppRegistry, Platform} from 'react-native';
 import {name as appName} from './app.json';
-import TrackPlayer from 'react-native-track-player';
-import PlaybackService from './PlaybackService';
 
-if (Platform.OS === 'android') {
-  AppRegistry.registerComponent(appName, () => App);
-  setupTrackPlayer();
-} else {
-  AppRegistry.registerComponent(appName, () => App);
-}
-
-TrackPlayer.registerPlaybackService(() => PlaybackService);
+AppRegistry.registerComponent(appName, () => App);
