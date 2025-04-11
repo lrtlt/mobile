0ab28cd7dbed286afdc85898483981534372641c
Kestas Venslauskas <kestas.venslauskas@gmail.com>
2025-04-12 00:23:58 +0300

    fix: android error

--- app/util/useAppTrackingPermission.ts
+++ app/util/useAppTrackingPermission.ts
@@ -1,9 +1,13 @@
 import {useEffect} from 'react';
+import {Platform} from 'react-native';
 import {check, request, PERMISSIONS} from 'react-native-permissions';
 
 const useAppTrackingPermission = () => {
   useEffect(() => {
     const checkForPermission = async () => {
+      if (Platform.OS !== 'ios') {
+        return;
+      }
       const checkResult = await check(PERMISSIONS.IOS.APP_TRACKING_TRANSPARENCY);
       console.log('APP_TRACKING_TRANSPARENCY check result: ', checkResult);
       if (checkResult === 'denied') {
