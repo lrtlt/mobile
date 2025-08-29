128b5b2ebb5d16c7bed9bfc27696b4aa709f790d
Kestas Venslauskas <kestas.venslauskas@gmail.com>
2025-08-29 15:47:53 +0300

    fix: chromecast for live stream

--- app/components/videoComponent/useChromecast.ts
+++ app/components/videoComponent/useChromecast.ts
@@ -3,6 +3,7 @@ import CastContext, {
   CastState,
   MediaHlsSegmentFormat,
   MediaHlsVideoSegmentFormat,
+  MediaInfo,
   MediaStatus,
   MediaStreamType,
   RemoteMediaClient,
@@ -39,30 +40,46 @@ const useChromecast = ({player, mediaType, isLiveStream, poster, streamUri, titl
     const subscription = CastContext.onCastStateChanged((castState) => {
       if (castState === CastState.CONNECTED) {
         CastContext.sessionManager.getCurrentCastSession().then((session) => {
-          console.log('streamUri', streamUri);
           const client = session?.client;
+          const isAudio = mediaType === MediaType.AUDIO;
+          const isLRT1 = streamUri.includes('lrt-portal-prod-01');
+
+          console.log('streamUri', streamUri);
+          console.log('isAudio', isAudio);
+          console.log('isLRT1', isLRT1);
+
           setClient(client);
           player.pause();
+
+          const startTime = player.currentTime / 1000;
+          const mediaInfo: MediaInfo = {
+            hlsSegmentFormat: isLRT1
+              ? MediaHlsSegmentFormat.AAC
+              : isLiveStream
+              ? isAudio
+                ? MediaHlsSegmentFormat.TS_AAC
+                : MediaHlsSegmentFormat.TS
+              : MediaHlsSegmentFormat.AAC,
+            hlsVideoSegmentFormat: MediaHlsVideoSegmentFormat.MPEG2_TS,
+            contentId: streamUri,
+            contentUrl: streamUri,
+            streamType: isLiveStream ? MediaStreamType.LIVE : MediaStreamType.BUFFERED,
+            contentType: 'application/vnd.apple.mpegurl',
+            metadata: {
+              type: isAudio ? 'musicTrack' : 'generic',
+              title: title,
+              images: [
+                {
+                  url: poster,
+                },
+              ],
+            },
+          };
+
           client?.loadMedia({
             autoplay: true,
-            startTime: player.currentTime / 1000,
-            mediaInfo: {
-              hlsSegmentFormat: isLiveStream ? MediaHlsSegmentFormat.TS : MediaHlsSegmentFormat.AAC,
-              hlsVideoSegmentFormat: MediaHlsVideoSegmentFormat.MPEG2_TS,
-              contentId: streamUri,
-              contentUrl: streamUri,
-              streamType: isLiveStream ? MediaStreamType.LIVE : MediaStreamType.BUFFERED,
-              contentType: 'application/vnd.apple.mpegurl',
-              metadata: {
-                type: mediaType == MediaType.AUDIO ? 'musicTrack' : 'generic',
-                title: title,
-                images: [
-                  {
-                    url: poster,
-                  },
-                ],
-              },
-            },
+            startTime: startTime,
+            mediaInfo: mediaInfo,
           });
         });
       } else {
