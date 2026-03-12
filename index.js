f7d0a854271366a9e1118fb2b83d21aeb322be5c
Kestas Venslauskas <kestas.venslauskas@gmail.com>
2026-03-12 16:14:03 +0200

    fix: iOS firebase crash

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ setupGemius();
 globalThis.RNFB_SILENCE_MODULAR_DEPRECATION_WARNINGS = true;
 // globalThis.RNFB_MODULAR_DEPRECATION_STRICT_MODE === true;
 
+// Eagerly initialize Firestore to avoid race condition where concurrent
+// calls to getFirestore() both try to set settings on the same instance.
+// See: https://github.com/invertase/react-native-firebase/issues/7496
+import {getFirestore} from '@react-native-firebase/firestore';
+getFirestore();
+
 import App from './App';
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
